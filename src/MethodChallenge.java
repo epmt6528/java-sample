4f351fb3d9d66d98f701c95be4ba83be21febe90
epmt6528 <epmt6528@gmail.com>
2023-06-26 09:10:24 -0700

    Solved method challenge

---
+++ src/MethodChallenge.java
@@ -0,0 +1,25 @@
+public class MethodChallenge {
+    public static void main(String[] args) {
+        displayHighScorePosition("Taichi", calculateHighScorePosition(1500));
+        displayHighScorePosition("Taichi", calculateHighScorePosition(1000));
+        displayHighScorePosition("Taichi", calculateHighScorePosition(500));
+        displayHighScorePosition("Taichi", calculateHighScorePosition(100));
+        displayHighScorePosition("Taichi", calculateHighScorePosition(25));
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score list.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if(1000 <= playerScore){
+            return 1;
+        }else if(500 <= playerScore){
+            return 2;
+        }else if(100 <= playerScore){
+            return 3;
+        }else{
+            return 4;
+        }
+    }
+}
