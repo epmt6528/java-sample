a74312f717e5d3308f67be9d544cdf605d57cc69
epmt6528 <epmt6528@gmail.com>
2023-11-27 09:26:55 -0800

    Implemented LargestPrime class

---
+++ src/LargestPrime.java
@@ -0,0 +1,24 @@
+public class LargestPrime {
+    public static int getLargestPrime(int number){
+        if(number <= 1) return -1;
+
+        int largestPrime = -1;
+        while (number % 2 == 0) {
+            largestPrime = 2;
+            number /= 2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            while (number % i == 0) {
+                largestPrime = i;
+                number /= i;
+            }
+        }
+
+        if (number > 2) {
+            largestPrime = number;
+        }
+
+        return largestPrime;
+    }
+}
